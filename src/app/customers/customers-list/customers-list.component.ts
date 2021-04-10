50ba6cbc203f2e840ce3edee20bcaef1c210a87a
John Thielman <john.thielman@gmail.com>
2021-04-09 21:44:11 -0500

    data passed from parent to child

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ICustomer } from '../../shared/interfaces';
 
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html',
-  styleUrls: ['./customers-list.component.scss']
 })
 export class CustomersListComponent implements OnInit {
+  private _customers: ICustomer[] = [];
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
+
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
+    }
+  }
   
   filteredCustomers: ICustomer[] = [];
   customersOrderTotal!: number;
@@ -26,5 +36,9 @@ export class CustomersListComponent implements OnInit {
         this.customersOrderTotal += cust.orderTotal;
       }
     });
+  }
+
+  sort(prop: string) {
+    // A sorter service will handle the sorting
   }
 }
